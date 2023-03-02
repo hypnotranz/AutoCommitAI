8533cfb155b23886cea6fade290c93b4915ab633
Richard Santomauro <richard.santomauro@nimbletec.com>
2023-03-01 22:44:06 -0500

    First commit

---
+++ code_diff.py
@@ -0,0 +1,34 @@
+import openai
+
+
+class CodexAPI:
+    def __init__(self, api_key, model_engine):
+        self.api_key = api_key
+        self.model_engine = model_engine
+        openai.api_key = self.api_key
+
+    def generate_diff_description(self, code1, code2, description):
+        prompt = f"Diff and describe changes:\n\nSnippet 1:\n{code1}\n\nSnippet 2:\n{code2}\n\nDescription:"
+
+        parameters = {
+            "engine": self.model_engine,
+            "prompt": prompt,
+            "max_tokens": 1000,
+            "temperature": 0.5,
+            "stop": None,
+        }
+
+        try:
+            response = openai.Completion.create(**parameters)
+            generated_code = ''
+            for choice in response.choices:
+                generated_code = choice.text
+            return generated_code
+        except openai.error.InvalidRequestError as e:
+            print(f"Invalid Request Error: {e}")
+        except openai.error.AuthenticationError as e:
+            print(f"Authentication Error: {e}")
+        except openai.error.APIConnectionError as e:
+            print(f"API Connection Error: {e}")
+        except openai.error.OpenAIError as e:
+            print(f"OpenAI Error: {e}")
--- main.py
+++ main.py
@@ -1,37 +1,12 @@
-import openai
+from codex_api import CodexAPI
 import os
 
-openai.api_key = "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"
-model_engine = "code-davinci-002"
+
+api = CodexAPI("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", "code-davinci-002")
 
 code1 = "def multiply(a, b):\n    return a * b"
 code2 = "def multiply(a, b):\n    if a < 0 or b < 0:\n        raise ValueError('Inputs must be positive')\n    return a * b"
+description = "Description of changes"
 
-# Define the prompt for the Codex API call
-prompt = f"Diff and describe changes:\n\nSnippet 1:\n{code1}\n\nSnippet 2:\n{code2}\n\nDescription:"
-
-# Define the parameters for the Codex API call
-parameters = {
-    "engine": model_engine,
-    "prompt": prompt,
-    "max_tokens": 1000,
-    "temperature": 0.5,
-    "stop": None,
-}
-
-try:
-    # Make the Codex API call
-    response = openai.Completion.create(**parameters)
-
-    # Print the generated code snippet
-    for choice in response.choices:
-        print(choice.text)
-
-except openai.error.InvalidRequestError as e:
-    print(f"Invalid Request Error: {e}")
-except openai.error.AuthenticationError as e:
-    print(f"Authentication Error: {e}")
-except openai.error.APIConnectionError as e:
-    print(f"API Connection Error: {e}")
-except openai.error.OpenAIError as e:
-    print(f"OpenAI Error: {e}")
+generated_code = api.generate_diff_description(code1, code2, description)
+print(generated_code)
